2be8ae3bd5a94d0ed1a7de5113d73ce7f2233f44
valchu <valentinpazm@gmail.com>
2014-10-25 11:48:44 -0300

    fix idiot

--- app/model/Geotag.java
+++ app/model/Geotag.java
@@ -13,7 +13,6 @@ import java.util.List;
 import static play.data.validation.Constraints.Required;
 
 @Entity
-@Table(name = "GEOTAGS")
 public class Geotag extends Model implements Comparable {
     public static boolean byLat = false;
 
